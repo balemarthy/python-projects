b75bd561a1ec4fea40e313896ee9d0c498de67fe
B Vamsi Krishna <krishna.bv@gmail.com>
2023-05-25 01:14:59 +0530

    Update Bagels.py
    Completed the remaining part of the program

--- Bagels.py
+++ Bagels.py
@@ -22,3 +22,67 @@ def main():
         secretNum = getSecretNum()
         print('I have thought up a number')
         print('You have {} guesses to get it.'.format(MAX_GUESSES))
+        
+        numGuesses = 1
+
+        while numGuesses <= MAX_GUESSES:
+            guess = ''
+            #keep looping until they enter a valid guess:
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print('Guess #{}:'.format(numGuesses))
+                guess = input('> ')
+
+            clues = getClues(guess, secretNum)
+            print(clues)
+            numGuesses += 1
+
+            if guess == secretNum:
+                break # They're correct, so break out of this loop
+
+            if numGuesses > MAX_GUESSES:
+                print('You ran out of guesses.')
+                print('The answer was {}.'.format(secretNum))
+
+            # Ask player if they want to play again.
+            print('Do you want to play again? (yes or no)')
+            if not input('> ').lower().startswith('y'):
+                break
+        print('Thanks for playing!')
+
+
+def getSecretNum():
+    """Returns a string made up of NUM_DIGITS unique random digits."""
+    numbers = list('0123456789') 
+    random.shuffle(numbers)
+
+    # Get the first NUM_DIGITS digits in the list for the secret number
+    secretNum = ''
+    for i in range(NUM_DIGITS):
+        secretNum += str(numbers[i])
+    return secretNum    
+                
+def getClues(guess, secretNum):
+    """Returns a string with pico, fermi, bagels clues for guess and secret number pair"""
+    if guess == secretNum:
+        return 'You got it!'
+    
+    clues = []
+
+    for i in range(len(guess)):
+        if guess[i] == secretNum[i]:
+            # A correct digit is in the correct place
+            clues.append('Fermi')
+        elif guess[i] in secretNum:
+            # A correct digit is in the incorrect place
+            clues.append('Pico')
+
+    if len(clues) == 0:
+        return 'Bagels' 
+    else:
+        clues.sort()
+        return ''.join(clues)
+
+# If the program is run, 
+if __name__ == '__main__':
+    main()           
+
